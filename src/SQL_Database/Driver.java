653928e77485c80b0119b051e518c1c694784880
Dhruv Rawat <dhruvrawat09@gmail.com>
2020-12-26 18:59:37 -0500

    Created all prepared statements for creating, updating, and deleting. Ready to move onto gui stage of development.

--- src/SQL_Database/Driver.java
+++ src/SQL_Database/Driver.java
@@ -2,7 +2,13 @@ package SQL_Database;
 
 import java.sql.*;
 
+import java.util.Scanner;
+
 public class Driver {
+	
+	
+	/* CREATE FUNCTIONS */
+	
 	
 	public static void create_employee(Connection myConn, String fname, String lname, String email) throws SQLException {
 		PreparedStatement create_employee_st = myConn.prepareStatement("INSERT INTO EMPLOYEE (employee_fname, employee_lname, employee_email) VALUES (?,?,?)");
@@ -41,6 +47,10 @@ public class Driver {
 		create_lesson_st.close();
 	}
 	
+	
+	/* DELETE FUNCTIONS */
+	
+	
 	public static void delete_employee(Connection myConn, String email) throws SQLException {
 		PreparedStatement delete_employee_st = myConn.prepareStatement("DELETE FROM employee WHERE employee_email=?");
 		
@@ -65,8 +75,142 @@ public class Driver {
 		delete_lesson_st.close();
 	}
 	
+	
+	/* SEARCH FUNCTIONS */
+	
+	
+	public static void search_employees(Connection myConn, String name) throws SQLException {
+		PreparedStatement search_employees_st = myConn.prepareStatement("SELECT * FROM employee WHERE employee_fname=?");
+		
+		search_employees_st.setString(1, name);
+		search_employees_st.executeQuery();
+	}
+	
+	public static void search_students(Connection myConn, String name) throws SQLException {
+		PreparedStatement search_student_st = myConn.prepareStatement("SELECT * FROM student WHERE client_fname=?");
+		
+		search_student_st.setString(1, name);
+		search_student_st.executeQuery();
+	}
+	
+	public static void search_lessons_from_student(Connection myConn, int id) throws SQLException {
+		PreparedStatement search_lesson_st = myConn.prepareStatement("SELECT * FROM lesson WHERE lesson_studentId=?");
+		
+		search_lesson_st.setInt(1, id);
+		search_lesson_st.executeQuery();
+	}
+	
+	public static void search_lessons_from_employee(Connection myConn, int id) throws SQLException {
+		PreparedStatement search_lesson_st = myConn.prepareStatement("SELECT * FROM lesson WHERE lesson_employeeId=?");
+		
+		search_lesson_st.setInt(1, id);
+		search_lesson_st.executeQuery();
+	}
+	
+	
+	/* UPDATE FUNCTIONS */
+	
+	
+	public static void update_employees(Connection myConn, String fname, String lname, String email, int id) throws SQLException {
+		PreparedStatement update_employee_st = myConn.prepareStatement("UPDATE employee SET employee_fname=?, employee_lname=?, employee_email=? WHERE employee_id=?");
+		
+		update_employee_st.setString(1, fname);
+		update_employee_st.setString(2, lname);
+		update_employee_st.setString(3, email);
+		update_employee_st.setInt(4, id);
+		// For CUD operations, make sure you use executeUpdate() and that you .close() it afterwards. For R operations, use executeQuery()
+		update_employee_st.executeUpdate();
+		update_employee_st.close();
+		
+	}
+	
+	public static void update_students(Connection myConn, String fname, String lname, String email, int plan, int id) throws SQLException {
+		PreparedStatement update_student_st = myConn.prepareStatement("UPDATE student SET client_fname=?, client_lname=?, client_email=?, client_plan=? WHERE employee_id=?");
+		
+		update_student_st.setString(1, fname);
+		update_student_st.setString(2, lname);
+		update_student_st.setString(3, email);
+		update_student_st.setInt(4, plan);
+		update_student_st.setInt(5, id);
+		// For CUD operations, make sure you use executeUpdate() and that you .close() it afterwards. For R operations, use executeQuery()
+		update_student_st.executeUpdate();
+		update_student_st.close();
+	
+	}
+	
+	public static void update_lessons(Connection myConn, int emp_id, int stu_id, Date date, int id) throws SQLException {
+		PreparedStatement update_lesson_st = myConn.prepareStatement("UPDATE lesson SET lesson_studentId=?, lesson_employee_id=?, lesson_date=?, WHERE lesson_id=?");
+
+		update_lesson_st.setInt(1, stu_id);
+		update_lesson_st.setInt(2, emp_id);
+		update_lesson_st.setDate(3, date);
+		update_lesson_st.setInt(4, id);
+		// For CUD operations, make sure you use executeUpdate() and that you .close() it afterwards. For R operations, use executeQuery()
+		update_lesson_st.executeUpdate();
+		update_lesson_st.close();
+	}
+	
+	
+	/* HELPERS */ 
+	
+	
+	private static boolean get_confirmation() {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Is it okay to save?  yes/no: ");
+		String input = scanner.nextLine();
+
+		scanner.close();
+
+		return input.equalsIgnoreCase("yes");
+	}
+	
+	
+	/* MISC */
+	
+	
+	// Just here for practice. Will not be used in final product
+	public static void practice_crud_operations(Connection myConn, Statement myStmt, ResultSet myRs) throws SQLException {
+		/* Print data */
+		
+		myRs = myStmt.executeQuery("select * from plan");
+		
+		while (myRs.next()) {
+			System.out.println(myRs.getString("plan_type"));
+		}
+		
+		/* Insert data */
+		
+		String sq1 = "insert into employee " + " (employee_fname, employee_lname, employee_email) " + " values ('Jim', 'Joe', 'jimjoe@gmail.com')";
+		myStmt.executeUpdate(sq1);
+		
+		String sq2 = "insert into employee " + " (employee_fname, employee_lname, employee_email) " + " values ('Tim', 'Cook', 'timcook@gmail.com')";
+		myStmt.executeUpdate(sq2);
+		
+		String sq3 = "insert into employee " + " (employee_fname, employee_lname, employee_email) " + " values ('Jeff', 'Ball', 'jeffball@gmail.com')";
+		myStmt.executeUpdate(sq3);
+		
+		// Make new student using prepared statement
+		create_employee(myConn, "Timmy", "Turner", "tt@gmail.com");
+		
+		/* Updating data */
+		
+		String sq4 = "update employee " + " set employee_email='aaaaa@gmail.com'" + " where employee_fname='Jeff'";
+		myStmt.executeUpdate(sq4);
+		
+		/* Delete data */ 
+		
+		String sq5 = "delete from employee where employee_fname='Jim'";
+		int affectedRows = myStmt.executeUpdate(sq5); // Executed statement and returns number of deleted rows (Don't need to return tho)
+		System.out.println("Affected rows: " + affectedRows); // Prints the number of rows that are affected (deleted)
+		
+	}
+	
+	
+	/* MAIN */
+	
  
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		
 		String url = "jdbc:mysql://localhost:3306/database_project";
 		String user = "root";
@@ -76,51 +220,32 @@ public class Driver {
 		ResultSet myRs = null;
 		Statement myStmt = null;
 		
-
 		try {
-			
 			// Get connection to database
 			myConn = DriverManager.getConnection(url, user, pass);
-
 			// create a statement
 			myStmt = myConn.createStatement();
-			
 			myRs = myStmt.executeQuery("select * from plan");
-
-
-			/* Print data */
 			
-//			while (myRs.next()) {
-//				System.out.println(myRs.getString("plan_type"));
-//			}
+//			practice_crud_operations(myConn, myStmt, myRs); // Used for practice
 			
-			/* Insert data */
-			
-//			String sq1 = "insert into employee " + " (employee_fname, employee_lname, employee_email) " + " values ('Jim', 'Joe', 'jimjoe@gmail.com')";
-//			myStmt.executeUpdate(sq1);
-//			
-//			String sq2 = "insert into employee " + " (employee_fname, employee_lname, employee_email) " + " values ('Tim', 'Cook', 'timcook@gmail.com')";
-//			myStmt.executeUpdate(sq2);
-//			
-//			String sq3 = "insert into employee " + " (employee_fname, employee_lname, employee_email) " + " values ('Jeff', 'Ball', 'jeffball@gmail.com')";
-//			myStmt.executeUpdate(sq3);
-			
-			// Make new student using prepared statement
-			create_employee(myConn, "Timmy", "Turner", "tt@gmail.com");
-			
-			/* Updating data */
-			
-//			String sq4 = "update employee " + " set employee_email='aaaaa@gmail.com'" + " where employee_fname='Jeff'";
-//			myStmt.executeUpdate(sq4);
-			
-			/* Delete data */ 
-			
-//			String sq5 = "delete from employee where employee_fname='Jim'";
-//			int affectedRows = myStmt.executeUpdate(sq5); // Executed statement and returns number of deleted rows (Don't need to return tho)
-//			System.out.println("Affected rows: " + affectedRows); // Prints the number of rows that are affected (deleted)
+			// Setting up interface for transactions
+			myConn.setAutoCommit(false); // This ensures that the user can confirm changes
 			
 			
 			
+			/* INSERT CHANGES/QUERIES HERE */	
+			update_employees(myConn, "D", "R", "d@gmail.com", 10);
+			
+			
+			
+			boolean confirmation = get_confirmation(); // Will make a button to toggle this value
+			
+			if (confirmation) {
+				myConn.commit();
+			} else {
+				myConn.rollback();
+			}
 		} catch (Exception exc) {
 			System.out.println("Failed");
 			exc.printStackTrace();
